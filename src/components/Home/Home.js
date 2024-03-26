dda4c199ce21c09101daf906b481fd34e696414a
jaganathan05 <jaganathanv888@gmail.com>
2024-03-26 15:24:37 +0530

    Rules of Hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,17 @@
-import React,{useContext} from 'react';
-import AuthContext from '../../store/auth-context';
-import Button from '../UI/Button/Button';
+import React, { useContext } from 'react';
+
 import Card from '../UI/Card/Card';
+import Button from '../UI/Button/Button';
 import classes from './Home.module.css';
+import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
-  const ctx = useContext(AuthContext);
+  const authCtx = useContext(AuthContext);
+
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={ctx.onLogout} >Logot</Button>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
